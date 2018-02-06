1560ba2a4904a1ea39a1081234947ea108e4c29e
Damien Levitt <damien.levitt@sjsu.edu>
2018-02-05 23:05:02 -0800

    Added a couple test cases.

--- test/lab01_tests.cpp
+++ test/lab01_tests.cpp
@@ -23,6 +23,8 @@ public:
 };
 
 
+
+
 TEST(crashTest , constructorTest) {
     expressionstream stream1 ("1+2+3");
     expressionstream stream2 ("1   + 2 *4+(2*3)");
@@ -74,8 +76,28 @@ TEST_F(Lab01Fixture, isNext) {
 //step 4: type name of your test
 //step 5: inside the function create your test, use previous test as examples
 
-TEST_F(Lab01Fixture, testTest) {
+TEST_F(Lab01Fixture, NotEqualToTwenty) {        //First test to see if first integer of stream4 is not equal to 20.
     expressionstream stream4("1+2*4");
 
-    EXPECT_EQ("1",stream4.get_next_int());
+    EXPECT_NE("20",stream4.get_next_int());
+}
+
+TEST_F(Lab01Fixture, EqualToZero) {             //Second test to see if first integer of stream5 is equal to 0.
+    expressionstream stream5("0+2+23");
+
+    EXPECT_EQ("0", stream5.get_next_int());
+}
+
+TEST_F(Lab01Fixture, NotEqualToMinus){          //Third test to determine if the first operator is not a minus.
+    EXPECT_NE("-", stream1->get_next_op());
+}
+
+TEST_F(Lab01Fixture, Stream1OperatorOrder) {
+    EXPECT_EQ("+", stream1->get_next_op());     //Fourth test tests if the first operator is a plus and if the second is not a minus.
+    EXPECT_NE("-", stream1->get_next_op());
+}
+
+TEST_F(Lab01Fixture, Stream3Integers) {
+    EXPECT_EQ("-1", stream3->get_next_int());   //Fifth test to determine if the two integers in stream3 are -1 and -2 in that exact order.
+    EXPECT_EQ("-2", stream3->get_next_int());
 }
