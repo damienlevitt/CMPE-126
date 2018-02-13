a773fd01daa6c3682b4b44b80553b0be1f2b8540
Damien Levitt <damien.levitt@sjsu.edu>
2018-02-13 10:53:26 -0800

    Merge branch 'master' of https://bitbucket.org/cmpe126s18/cmpe126s18_labs
    # Conflicts:
    #	lib/lab02/inc/stringVector.h
    #	lib/lab02/src/stringVector.cpp
    #	test/lab01_tests.cpp

--- lib/lab02/src/stringVector.cpp
+++ lib/lab02/src/stringVector.cpp
@@ -1,77 +1,81 @@
 #include "../inc/stringVector.h"
+namespace lab2 {
 
+    stringVector::stringVector() {
+        length;
+        allocated_length;
+        data = nullptr;
+    }
 
-stringVector::stringVector() {
-    length;
-    allocated_length;
-    data = nullptr;
-}
+    stringVector::~stringVector() {
+        delete[] data;
+    }
 
-stringVector::~stringVector() {
-    delete[] data;
-}
+    unsigned stringVector::size() const {
+        return length;
 
-unsigned stringVector::size() const{
-    return length;
+        //return ;
+    }
 
-    //return ;
-}
+    unsigned stringVector::capacity() const {
+        return allocated_length;
+        //return ;
+    }
 
-unsigned stringVector::capacity() const{
-    return allocated_length;
-    //return ;
-}
-
-void stringVector::reserve(unsigned new_size) {
-        std::string *data = new std::string int[new_size];
+    void stringVector::reserve(unsigned new_size) {
+        std::string *data = new std::string[new_size];
         delete data;
-}
-
-bool stringVector::empty() const{
-    if(length != 0)
-        return true;
-    else
-        return false;
-
-    //return ;
-}
-
-void stringVector::append(std::string new_data) {
-    if(empty() == false) {
-        this->reserve(1);
     }
-    else{
-        for(int counter = 0; counter < allocated_length; counter++){
-         data[length++] = new_data[counter + 1];
+
+    bool stringVector::empty() const {
+        if (length != 0)
+            return true;
+        else
+            return false;
+
+        //return ;
+    }
+
+    void stringVector::append(std::string new_data) {
+        if (empty() == false) {
+            this->reserve(1);
+            this->append(new_data);
         }
-        int temp = 2*allocated_length;
-        allocated_length = temp;
+        else if (allocated_length == length) {
+            this->reserve(allocated_length * 2);
+            this->append(new_data);
+        }
+        else{
+            data[length] = new_data;
+            length++;
+            return;
     }
 }
-
 void stringVector::swap(unsigned pos1, unsigned pos2) {
-    if(pos1 > allocated_length || pos2 > allocated_length) {
-
+        if (pos1 > allocated_length || pos2 > allocated_length) {
+                throw 0;
+        }
+        std::string tempswap = data[pos1];
+        data[pos1] = data[pos2];
+        data[pos2] = tempswap;
     }
-}
 
-stringVector &stringVector::operator=(stringVector const &rhs) {
-   if(&rhs == this){
-       return (*this);
-   }
-    else {
-       delete[] data;
-       length = rhs.length;
-       allocated_length = rhs.allocated_length;
-       this->data = new std::string int[allocated_length];
-       for(int i = 0; i<length; i++) {
-           this->data[i] = rhs.data[i];
-       }
-   }
-    //return ;
-}
+    stringVector &stringVector::operator=(stringVector const &rhs) {
+        if (&rhs == this) {
+            return (*this);
+        } else {
+            delete[] data;
+            length = rhs.length;
+            allocated_length = rhs.allocated_length;
+            this->data = new std::string[allocated_length];
+            for (int i = 0; i < length; i++) {
+                this->data[i] = rhs.data[i];
+            }
+        }
+        //return ;
+    }
 
-std::string &stringVector::operator[](unsigned position) {
+    std::string &stringVector::operator[](unsigned position) {
         if (position >= allocated_length) {
             throw 0;
         }
@@ -79,16 +83,17 @@ std::string &stringVector::operator[](unsigned position) {
     }
 
 
-void stringVector::sort() {
-    std::string string1;
-    for(int i = (length - 1); i > 0; i--){
-        for(int n = 0; n < i; n++){
-            if(data[n].compare(data[n + 1]) > 0){
-                string1 = data[n];
-                data[n] = data[n + 1];
-                data[n+1] = string1;
+    void stringVector::sort() {
+        std::string string1;
+        for (int i = (length - 1); i > 0; i--) {
+            for (int n = 0; n < i; n++) {
+                if (data[n].compare(data[n + 1]) > 0) {
+                    string1 = data[n];
+                    data[n] = data[n + 1];
+                    data[n + 1] = string1;
+                }
             }
         }
-    }
 
+    }
 }
--- test/lab01_tests.cpp
+++ test/lab01_tests.cpp
@@ -1,70 +1,70 @@
 #include "gtest/gtest.h"
 #include "expressionstream.h"
+namespace lab1 {
+    class Lab01Fixture : public ::testing::Test {
 
-class Lab01Fixture : public ::testing::Test {
+    protected:
+        virtual void TearDown() {
+            delete stream1;
+            delete stream2;
+        }
 
-protected:
-    virtual void TearDown() {
-        delete stream1;
-        delete stream2;
+        virtual void SetUp() {
+            stream1 = new expressionstream("1+2+3");
+            stream2 = new expressionstream("1   + 2 *4+(2*3)");
+            stream3 = new expressionstream("-1 +-2");
+        }
+
+
+    public:
+        expressionstream *stream1;
+        expressionstream *stream2;
+        expressionstream *stream3;
+    };
+
+
+    TEST(crashTest, constructorTest) {
+        expressionstream stream1("1+2+3");
+        expressionstream stream2("1   + 2 *4+(2*3)");
     }
 
-    virtual void SetUp() {
-        stream1=new expressionstream("1+2+3");
-        stream2=new expressionstream("1   + 2 *4+(2*3)");
-        stream3=new expressionstream("-1 +-2");
-    }
 
+    TEST_F(Lab01Fixture, basicTest) {
+        EXPECT_EQ("1", stream1->get_next_int());
+        EXPECT_EQ("2", stream1->get_next_int());
+        EXPECT_EQ("3", stream1->get_next_int());
 
-public:
-    expressionstream *stream1;
-    expressionstream *stream2;
-    expressionstream *stream3;
-};
-
-
-TEST(crashTest , constructorTest) {
-    expressionstream stream1 ("1+2+3");
-    expressionstream stream2 ("1   + 2 *4+(2*3)");
-}
-
-
-TEST_F(Lab01Fixture, basicTest) {
-    EXPECT_EQ("1",stream1->get_next_int());
-    EXPECT_EQ("2",stream1->get_next_int());
-    EXPECT_EQ("3",stream1->get_next_int());
-
-    EXPECT_EQ("+",stream1->get_next_op());
-    EXPECT_EQ("+",stream1->get_next_op());
+        EXPECT_EQ("+", stream1->get_next_op());
+        EXPECT_EQ("+", stream1->get_next_op());
 //todo: Get these tests to pass using the debugger
-    EXPECT_EQ("1",stream2->get_next_int());
-    EXPECT_EQ("2",stream2->get_next_int());
-    EXPECT_EQ("4",stream2->get_next_int());
-    EXPECT_EQ("2",stream2->get_next_int());
-    EXPECT_EQ("3",stream2->get_next_int());
+        EXPECT_EQ("1", stream2->get_next_int());
+        EXPECT_EQ("2", stream2->get_next_int());
+        EXPECT_EQ("4", stream2->get_next_int());
+        EXPECT_EQ("2", stream2->get_next_int());
+        EXPECT_EQ("3", stream2->get_next_int());
 
-    EXPECT_EQ("+",stream2->get_next_op());
-    EXPECT_EQ("*",stream2->get_next_op());
-    EXPECT_EQ("+",stream2->get_next_op());
-    EXPECT_EQ("(",stream2->get_next_op());
-    EXPECT_EQ("*",stream2->get_next_op());
-    EXPECT_EQ(")",stream2->get_next_op());
+        EXPECT_EQ("+", stream2->get_next_op());
+        EXPECT_EQ("*", stream2->get_next_op());
+        EXPECT_EQ("+", stream2->get_next_op());
+        EXPECT_EQ("(", stream2->get_next_op());
+        EXPECT_EQ("*", stream2->get_next_op());
+        EXPECT_EQ(")", stream2->get_next_op());
 
-    EXPECT_EQ("+", stream3->get_next_op());
-    EXPECT_EQ("-1", stream3->get_next_int());
-    EXPECT_EQ("-2", stream3->get_next_int());
-}
+        EXPECT_EQ("+", stream3->get_next_op());
+        EXPECT_EQ("-1", stream3->get_next_int());
+        EXPECT_EQ("-2", stream3->get_next_int());
+    }
 
-TEST_F(Lab01Fixture, isNext) {
-    EXPECT_FALSE(stream1->expression_complete());
-    stream1->get_next_int();
-    stream1->get_next_int();
-    stream1->get_next_int();
-    EXPECT_FALSE(stream1->expression_complete());
-    stream1->get_next_op();
-    stream1->get_next_op();
-    EXPECT_TRUE(stream1->expression_complete());
-}
+    TEST_F(Lab01Fixture, isNext) {
+        EXPECT_FALSE(stream1->expression_complete());
+        stream1->get_next_int();
+        stream1->get_next_int();
+        stream1->get_next_int();
+        EXPECT_FALSE(stream1->expression_complete());
+        stream1->get_next_op();
+        stream1->get_next_op();
+        EXPECT_TRUE(stream1->expression_complete());
+    }
 
 
 //todo: create a new test
@@ -74,8 +74,9 @@ TEST_F(Lab01Fixture, isNext) {
 //step 4: type name of your test
 //step 5: inside the function create your test, use previous test as examples
 
-TEST_F(Lab01Fixture, testTest) {
-    expressionstream stream4("1+2*4");
+    TEST_F(Lab01Fixture, testTest) {
+        expressionstream stream4("1+2*4");
 
-    EXPECT_EQ("1",stream4.get_next_int());
+        EXPECT_EQ("1", stream4.get_next_int());
+    }
 }
