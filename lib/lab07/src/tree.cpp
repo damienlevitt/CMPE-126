3279c48e1324b05d4530d8f7f6bad9b32dc99a47
Damien Levitt <damien.levitt@sjsu.edu>
2018-05-22 18:06:45 -0700

    Working on lab 7 function implementations

--- lib/lab07/src/tree.cpp
+++ lib/lab07/src/tree.cpp
@@ -1,11 +1,18 @@
 #include "../inc/tree.h"
-#include <iostream>
 
 namespace lab7 {
     void clear(node *to_clear);
     int recursive_level(node* top, int location);
     void recursive_insert(node* top, int value);
     node* find_node(node* top, int key);
+    node* find_parent(node* top, node* target);
+    bool has_children(node* target);
+    node* calc_swap(node* top);
+    void recursive_path_to(node* top, int key);
+    unsigned recursive_size(node* top);
+    unsigned recursive_depth(node* top);
+    bool recursive_in_tree(node* top, int key);
+
 
     // Construct an empty tree
     tree::tree() {
@@ -15,7 +22,6 @@ namespace lab7 {
 
     // Copy constructor
     tree::tree(const tree &copy) {
-
     }
 
     // Deconstruct tree
@@ -36,14 +42,39 @@ namespace lab7 {
 
     // Remove key return true if the key is deleted, and false if it isn't in the tree
     bool tree::remove(int key) {
-        if(in_tree(key)){
-            node *target = find_node(root, key);
+        if(!(in_tree(key))){
+            node* target = find_node(root, key);
             if(target->frequency > 1){
                 target->frequency--;
                 return true;
             }
+            node* target_parent = find_parent(root, target);
+            if (has_children(target)) {
+                node *target_swap = calc_swap(target);
+                node *parent_swap = find_parent(root, target_swap);
+
+                if (has_children(target_swap)) parent_swap->right = target_swap->left;
+                else parent_swap->right = nullptr;
+
+                target_swap->left = target->left;
+                target_swap->right = target->right;
+                if(target_parent != nullptr) {
+                    if (target_parent->left == target) target_parent->left = target_swap;
+                    else target_parent->right = target_swap;
+                }
+                else root = target_swap;
+            }
+            else {
+                if(target_parent != nullptr) {
+                    if (target_parent->left == target) target_parent->left = nullptr;
+                    else target_parent->right = nullptr;
+                }
+                else root = nullptr;
+            }
+            delete target;
+            return true;
         }
-       // node* target_parent =
+        else return false;
     }
 
     // What level is key on?
@@ -58,22 +89,22 @@ namespace lab7 {
 
     // Print the path to the key, starting with root
     void tree::path_to(int key) {
-
+        recursive_path_to(root, key);
     }
 
     // Number of items in the tree
     unsigned tree::size() {
-
+        recursive_size(root);
     }
 
     // Calculate the depth of the tree, longest string of connections
     unsigned tree::depth() {
-
+        recursive_depth(root);
     }
 
     // Determine whether the given key is in the tree
     bool tree::in_tree(int key) {
-
+        recursive_in_tree(root, key);
     }
 
     // Return the number of times that value is in the tree
@@ -103,15 +134,15 @@ namespace lab7 {
     }
 
     //Auxiliary function implementations//
-    int recursive_level(node* top, int key){
-        if(top->data == key){
+    int recursive_level(node* top, int location){
+        if(top->data == location){
             return 0;
         }
-        else if(key < top->data){                                   //if key is less than top node data go left
-            return(recursive_level(top->left, key) + 1);
+        else if(location < top->data){                                   //if location is less than top node data go left
+            return(recursive_level(top->left, location) + 1);
         }
-        else if (key > top->data){                                  //if key is greater than top node data goes right
-            return(recursive_level(top->right, key) +1);
+        else if (location > top->data){                                  //if location is greater than top node data goes right
+            return(recursive_level(top->right, location) +1);
         }
     }
 
@@ -145,8 +176,93 @@ namespace lab7 {
                 return(find_node(top->right, key));
             }
         }
+        node* find_parent(node* top, node* target){
+            if(top != target){
+                if(top->left == target || top->right == target){
+                    return top;
+                }
+                else if (target->data < top->data){
+                        return find_parent(top->left, target);
+                    }
+                else if (target->data > top->data){
+                    return find_parent(top->right,  target);
+                }
 
+                }
+             else
+                return top;
+        }
 
+    bool has_children(node* target){
+        if (!(target->left != nullptr || target->right != nullptr)){
+            return false;
+        }
+        else return true;
+    }
+
+    node* calc_swap(node* top){
+        if(!(top->left)){
+            top = top->right;
+            return top;
+        }
+        else
+            top = top->left;
+            while(top->right){
+                top = top->right;
+                return top;
+            }
+    }
+    void recursive_path_to(node* top, int key){
+        std::cout << top->data;
+        if(!(top->data == key)){
+            std::cout << " -> ";
+            if(key < top->data){
+                recursive_path_to(top->left, key);
+            }
+            else recursive_path_to(top->right, key);
+        }
+        else{
+            if(top->data == key){
+                std::cout << std::endl;
+                return;
+            }
+        }
+    }
+
+    unsigned recursive_size(node* top){
+        if(top != nullptr){
+            return top->frequency + recursive_size(top->left) + recursive_size(top->right);
+        }
+        else
+            return 0;
+    }
+
+    unsigned recursive_depth(node* top){
+        if(top == nullptr){
+            return 0;
+        }
+        unsigned left = 0;
+        unsigned right = 0;
+        left = 1 + recursive_depth(top->left);
+        right = 1 + recursive_depth(top->right);
+        if(right < left){
+            return left;
+        }
+        else return right;
+    }
+    bool recursive_in_tree(node* top, int key){
+        if(top->data == key){
+            return true;
+        }
+        else if(key < top->data){
+            return recursive_in_tree(top->left, key);
+        }
+        else if(key > top->data){
+            return recursive_in_tree(top->right, key);
+        }
+        else
+            return false;
+    }
     /**************************
      * Extra credit functions *
      **************************/
